a093353bd467171e14a1842bc00c6a6efee63bd0
Michael Anokye <michaelanokyej@yahoo.com>
2019-08-19 21:39:46 -0400

    to do project completed

--- README.md
+++ README.md
@@ -5,4 +5,8 @@
 
 # What I Learned
 
-* Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur tempore alias doloremque ducimus beatae consequatur quaerat, neque, ut possimus sint eveniet animi minima dolor accusamus dignissimos dicta harum aperiam sed?
+* Developed a user interface within the xml file using RelativeLayout, ListView, EditText, & Button.
+* Implemented functinality to the UI: onCreate, onAddItem, setupListViewListener, readItems, & writeItems.
+* Integrated setOnItemLongClickListener to remove items from the list.
+
+*All the above are placeholders
--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,33 @@
-alert('connected');
+// Check off specific Todos by clicking
+$('ul').on('click', 'li', function(){
+// Toggle the completed class
+	$(this).toggleClass('completed');
+});
+
+// Click on X to delete todo
+$('ul').on('click', 'span', function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$('input[type="text"]').keypress(function(event){
+// Check if user has hit enter
+	if(event.which === 13){
+// Grab new todo text from input
+	let toDoText = $(this).val();
+// Clean input after grabbing value
+	$(this).val('');
+// create a new li and add to ul
+	$('ul').append(`<li><span><i class="fas fa-trash-alt"></i></span> ${toDoText}</li>`);
+	}
+});
+
+
+$('.fa-plus').click(function(){
+	$('input[type="text"]').fadeToggle();
+});
+
+
+
